7c117d3f1406b584b445d6bd9890205077285849
Leonardo Sales <leonardosales.silva12@gmail.com>
2022-11-23 21:48:09 -0300

    updates

---
+++ main.js
@@ -0,0 +1,29 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 300
+}).reveal(`
+  .scrollReveal-top
+`)
+ScrollReveal({
+  origin: 'right',
+  distance: '100px',
+  duration: 300
+}).reveal(`
+  .scrollReveal-right
+`)
+ScrollReveal({
+  origin: 'left',
+  distance: '100px',
+  duration: 300
+}).reveal(`
+  .scrollReveal-left
+`)
+ScrollReveal({
+  origin: 'bottom',
+  distance: '100px',
+  duration: 300
+}).reveal(`
+  .scrollReveal-bottom
+`)
+
