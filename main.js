ab0321f5d11309381cf06de37e3d06ba24f5499a
Leonardo Sales <leonardosales.silva12@gmail.com>
2022-11-24 22:47:16 -0300

    scroll reveal adjust

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 ScrollReveal({
   origin: 'top',
   distance: '100px',
-  duration: 300
+  duration: 1300
 }).reveal(`
   .scrollReveal-top
 `)
 ScrollReveal({
   origin: 'right',
   distance: '100px',
-  duration: 300
+  duration: 1300
 }).reveal(`
   .scrollReveal-right
 `)
 ScrollReveal({
   origin: 'left',
   distance: '100px',
-  duration: 300
+  duration: 1300
 }).reveal(`
   .scrollReveal-left
 `)
 ScrollReveal({
   origin: 'bottom',
   distance: '100px',
-  duration: 300
+  duration: 1300
 }).reveal(`
   .scrollReveal-bottom
 `)
